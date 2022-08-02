4f92c1e99e1afe46c664e1dd0298236f89927e8b
Joaquin Lamela <joaquin.lamela00@gmail.com>
2022-08-02 15:48:28 -0300

    Add form for login and footer

--- api-medic/src/App.js
+++ api-medic/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/SideBar";
 import Register from "./containers/register";
+import Login from "./containers/login";
+import Footer from "./components/Footer";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,7 +20,9 @@ function App() {
       <Navbar openSidebar={toggle} />
       <Routes>
         <Route path="/signup" element={<Register />} exact />
+        <Route path="/login" element={<Login />} exact />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
